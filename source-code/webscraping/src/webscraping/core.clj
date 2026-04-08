0c8dcd269ffd4b0babe8412b1c6628cdf9d4749e
Mark Watson <markw@markwatson.com>
2026-04-08 09:57:58 -0700

    added exception handling

--- source-code/webscraping/src/webscraping/core.clj
+++ source-code/webscraping/src/webscraping/core.clj
@@ -18,28 +18,35 @@
 (defn fetch-web-page-data
   "Get the <a> anchor data and full text from a web URI"
   [a-uri]
-  (let [doc
-        (->
-          (. Jsoup connect a-uri)
-          (.userAgent
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.0; rv:77.0) Gecko/20100101 Firefox/77.0")
-          (.timeout 20000)
-          (.get))
-        all-page-text (. doc text)
-        anchors (get-html-anchors doc)]
-    {:page-text all-page-text :anchors anchors}))
+  (try
+    (let [doc
+          (->
+            (. Jsoup connect a-uri)
+            (.userAgent
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.0; rv:77.0) Gecko/20100101 Firefox/77.0")
+            (.timeout 20000)
+            (.get))
+          all-page-text (. doc text)
+          anchors (get-html-anchors doc)]
+      {:page-text all-page-text :anchors anchors})
+    (catch Exception e
+      (binding [*out* *err*] (println (str "Error fetching page " a-uri ": " (.getMessage e))))
+      {:page-text nil :anchors []})))
 
 (defn -main [& args]
   (println "Fetching data from https://markwatson.com...")
-  (let [page-data (fetch-web-page-data "https://markwatson.com")
-        anchors (:anchors page-data)]
-    (if (seq anchors)
-      (do
-        (println (str "\nFound " (count anchors) " anchors:"))
-        (println "\nFirst 5 anchors:")
-        (doseq [anchor (take 5 anchors)]
-          (println (str "  Text: " (:text anchor)))
-          (println (str "  URI:  " (:uri anchor)))
-          (println "----")))
-      (println "\nNo anchors found on the page."))
-    (println "\nFetching complete.")))
+  (try
+    (let [page-data (fetch-web-page-data "https://markwatson.com")
+          anchors (:anchors page-data)]
+      (if (seq anchors)
+        (do
+          (println (str "\nFound " (count anchors) " anchors:"))
+          (println "\nFirst 5 anchors:")
+          (doseq [anchor (take 5 anchors)]
+            (println (str "  Text: " (:text anchor)))
+            (println (str "  URI:  " (:uri anchor)))
+            (println "----")))
+        (println "\nNo anchors found on the page."))
+      (println "\nFetching complete."))
+    (catch Exception e
+      (binding [*out* *err*] (println (str "Unexpected error: " (.getMessage e)))))))
